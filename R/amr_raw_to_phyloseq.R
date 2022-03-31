784617fd864eb0d5c86d48bb31741216c1312716
mweinroth <maggie.weinroth@usda.gov>
2022-03-30 20:53:32 -0400

    updated amr_raw_to_phyloseq and NAMESPACE

--- R/amr_raw_to_phyloseq.R
+++ R/amr_raw_to_phyloseq.R
@@ -1,23 +1,23 @@
 ##' Raw AMR files plus metadata to phyloseq object
 ##'@name amr_raw_to_phyloseq
 ##' @description given directory and metadata make phyloseq object \pkg{\link{phyloseq}} package required.
-##' @param x Data.
-##' @param y More data.
+##' @param path.to.files path to data of raw csv files from AMRA CARD analysis
+##' @param metdata data.table of metadata with "filename" and "barcode"  columns required
+#' @param coveragenumber Minimum percentage of a gene that must be covered 0 to 99, default = 80
+#' @param keepSNP true or false to keep AMR gene conferred by one SNP change, default = FALSE
 ##' @seealso \pkg{\link{phyloseq}}
-##' @export
-#' @param amr.count.table count of AMR genes with CVTERMID in the first row and samples on columns
-#' @param verbose true or false to keep all columns of AMR gene information or not
-#' @return data table of taxonomy of AMR genes in count table
+#' @return phyloseq object for downstream analysis
 #' @examples
 #' \dontrun{
-#' generate_amr_taxonomy(amr.count.table = amr.count.table, verbose = FALSE)
+#' amr_raw_to_phyloseq(amr.count.table = path/to/amr.count.table,
+#' metadata = metadata, coveragenumber = 80, keepSNP = FALSE)
 #' }
 #' @import data.table
+#' @importFrom  phyloseq phyloseq
+#' @importFrom  phyloseq otu_table
+#' @importFrom phyloseq tax_table
+#' @importFrom  phyloseq sample_data
 #' @export
-
-data(CARD_taxonomy, envir=environment())
-
-
 
 data(CARD_taxonomy, envir=environment())
 
@@ -67,28 +67,31 @@ amr_raw_to_phyloseq <- function(path.to.files, metadata, coveragenumber = 80, ke
   }
   #remove mis-barcoded samples
   metadata$sampleID <- paste(metadata$filename, metadata$barcode, sep = "_")
-  # sampleID_names <- metadata[,"sampleID"]
-  # amr_count_table.t <- as.data.table(t(as.matrix(csvtry2, rownames = "CVTERMID")), keep.rownames = "sampleID")
-  #dropped_mis_barcode.t <- merge(x = sampleID_names, amr_count_table.t,by = "sampleID", all.x = TRUE)
-  # dropped_mis_barcode <- as.data.table(t(dropped_mis_barcode.t), keep.rownames = TRUE)
-  #colnames(dropped_mis_barcode) <- dropped_mis_barcode[1,]
-  # dropped_mis_barcode_table <- dropped_mis_barcode[-1, ]
-
+  sampleID_names <- metadata[,"sampleID"]
+  amr_count_table.t <- as.data.table(t(as.matrix(amr_count_table, rownames = "CVTERMID")), keep.rownames = "sampleID")
+  dropped_mis_barcode.t <- merge(x = sampleID_names, amr_count_table.t,by = "sampleID", all.x = TRUE)
+  #entire section because I could not get the df to be numeric in the count data
+  dropped_mis_barcode <- as.data.frame(t(dropped_mis_barcode.t))
+  dropped_col_names <- as.character(dropped_mis_barcode[1,])
+  dropped_mis_barcode_table <- dropped_mis_barcode[-1, ]
+  dropped_row_names <- as.character(row.names(dropped_mis_barcode_table))
+  amr_table_numeric <- as.data.frame(sapply(dropped_mis_barcode_table, as.numeric))
+  rownames(amr_table_numeric) <- dropped_row_names
+  colnames(amr_table_numeric) <- dropped_col_names
   #taxonomy generation
-  amr.CVTERMID.list <- amr_count_table[,1]
+  amr.CVTERMID.list <- as.data.frame(row.names(amr_table_numeric))
+  setnames(amr.CVTERMID.list, "row.names(amr_table_numeric)", "CVTERMID")
   amr.CVTERMID.list$CVTERMID <- as.numeric(amr.CVTERMID.list$CVTERMID)
   CARD_taxonomy$CVTERMID <- as.numeric(CARD_taxonomy$CVTERMID)
   merged.data <- merge(x = amr.CVTERMID.list, y = CARD_taxonomy, by = "CVTERMID", all.x = TRUE)
-  taxa_short <- merged.data[, c("CVTERMID","Drug Class", "AMR Gene Family", "Resistance Mechanism", "ARO Name")]
-
+  taxa_short <- merged.data[, c("Drug Class", "AMR Gene Family", "Resistance Mechanism", "ARO Name")]
+  rownames(taxa_short) <- dropped_row_names
+  #quick metadata
+  metadata <- as.data.frame(metadata)
+  rownames(metadata) <- metadata$sampleID
   #put it together
-  OTU = otu_table(amr_count_table, taxa_are_rows = TRUE)
+  OTU = otu_table(amr_table_numeric, taxa_are_rows = TRUE)
   TAX = tax_table(as.matrix(taxa_short))
   META = sample_data(metadata)
-  phyloseq(OTU, TAX)
-
+  print(phyloseq(OTU, TAX, META))
 }
-
-#phyloseq_object <- amr_raw_to_phyloseq(path.to.files = "raw-amr/", metadata = metadata)
-
-#metadata <- read_csv("meta-amr.csv")
