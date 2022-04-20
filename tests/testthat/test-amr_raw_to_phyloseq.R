74598acf8cdae57cfb38707d2db1d20ac40258b9
mweinroth <maggie.weinroth@usda.gov>
2022-04-20 09:50:27 -0400

    fixing description author line

--- tests/testthat/test-amr_raw_to_phyloseq.R
+++ tests/testthat/test-amr_raw_to_phyloseq.R
@@ -1,3 +1,4 @@
+library(epi2me2r)
 test_that("amr_raw_to_phyloseq gives phyloseq object", {
   metadata <- read.csv("../../inst/example_metadata.csv")
   df.test <- amr_raw_to_phyloseq(path.to.amr.files = "../../inst/example_amr_data/",
--- tests/testthat/test-read_in_amr_files.R
+++ tests/testthat/test-read_in_amr_files.R
@@ -1,3 +1,4 @@
+library(epi2me2r)
 test_that("read_in_amr works", {
   df.test <- read_in_amr_files(path.to.amr.files = "../../inst/example_amr_data/")
   expect_s3_class(df.test, "data.frame")
