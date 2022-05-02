88dc74f57f104b3bbad31ebe7e1134fdfa4437be
mweinroth <maggie.weinroth@usda.gov>
2022-05-02 13:27:34 -0400

    finally got tests working

--- tests/testthat/test-amr_raw_to_phyloseq.R
+++ tests/testthat/test-amr_raw_to_phyloseq.R
@@ -1,7 +1,7 @@
 library(epi2me2r)
 test_that("amr_raw_to_phyloseq gives phyloseq object", {
-  metadata <- read.csv("../../inst/example_metadata.csv")
-  df.test <- amr_raw_to_phyloseq(path.to.amr.files = "../../inst/example_amr_data/",
+  metadata <- read.csv("testdata/example_metadata.csv")
+  df.test <- amr_raw_to_phyloseq(path.to.amr.files = "testdata/example_amr_data/",
                                  metadata = metadata)
   expect_s4_class(df.test, "phyloseq")
 })
--- tests/testthat/test-read_in_amr_files.R
+++ tests/testthat/test-read_in_amr_files.R
@@ -1,6 +1,6 @@
 library(epi2me2r)
 system.file(package="epi2me2r")
 test_that("read_in_amr works", {
-  df.test <- read_in_amr_files(path.to.amr.files = "../../inst/example_amr_data/")
+  df.test <- read_in_amr_files(path.to.amr.files = "testdata/example_amr_data/")
   expect_s3_class(df.test, "data.frame")
 })
