b2c09fc83b51a79e0cfd0b07bbcf8f9177d3f2a9
mweinroth <maggie.weinroth@usda.gov>
2022-05-02 10:39:02 -0400

    moved example data to inst/ext/data

--- tests/testthat/test-read_in_amr_files.R
+++ tests/testthat/test-read_in_amr_files.R
@@ -1,4 +1,5 @@
 library(epi2me2r)
+system.file(package="epi2me2r")
 test_that("read_in_amr works", {
   df.test <- read_in_amr_files(path.to.amr.files = "../../inst/example_amr_data/")
   expect_s3_class(df.test, "data.frame")
