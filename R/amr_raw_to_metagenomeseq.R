362b4cac78fde5ac4399803d6c6db1e2e815c9b9
Quentin Read <quentin.read@usda.gov>
2022-05-03 12:00:15 -0400

    Replace entire contents of metagenomeseq functions with one call to phyloseq

--- R/amr_raw_to_metagenomeseq.R
+++ R/amr_raw_to_metagenomeseq.R
@@ -32,49 +32,6 @@ data(CARD_taxonomy, envir=environment())
 amr_raw_to_metagenomeseq <- function(path.to.amr.files, metadata,
                                      coveragenumber=80, keepSNP=FALSE){
 
-  # Checks for valid input. Fails with error if any are not met.
-  stopifnot(coveragenumber >= 0 & coveragenumber <= 99)
-  stopifnot(is.logical(keepSNP))
-  stopifnot(dir.exists(path.to.amr.files))
-  stopifnot(is.data.frame(metadata))
-
-  if (any(!c('arma_filename', 'amra_barcode') %in% names(metadata))) {
-    stop('metadata does not have columns named "arma_filename" and "amra_barcode".')
-  }
-
-  amr_count_table <- read_in_amr_files(path.to.amr.files, coveragenumber, keepSNP)
-
-  #remove mis-barcoded samples
-  metadata$sampleID <- paste(metadata$arma_filename,
-                             metadata$amra_barcode, sep = "_")
-  sampleID_names <- as.data.frame(metadata$sampleID)
-  colnames(sampleID_names) <- "sampleID"
-  amr_count_table.t <- as.data.table(t(as.matrix(amr_count_table,
-                                                 rownames = "CVTERMID")),
-                                     keep.rownames = "sampleID")
-  dropped_mis_barcode.t <- merge(x = sampleID_names,
-                                 amr_count_table.t,by = "sampleID",
-                                 all.x = TRUE)
-  #entire section because I could not get the df to be numeric in the count data
-  dropped_mis_barcode <- as.data.frame(t(dropped_mis_barcode.t))
-  dropped_col_names <- as.character(dropped_mis_barcode[1,])
-  dropped_mis_barcode_table <- dropped_mis_barcode[-1, ]
-  dropped_row_names <- as.character(row.names(dropped_mis_barcode_table))
-  amr_table_numeric <- as.data.frame(sapply(dropped_mis_barcode_table,
-                                            as.numeric))
-  rownames(amr_table_numeric) <- dropped_row_names
-  colnames(amr_table_numeric) <- dropped_col_names
-
-  #taxonomy generation
-  taxa_short <- generate_amr_taxonomy(amr_count_table, verbose = FALSE)
-
-  #quick metadata
-  metadata <- as.data.frame(metadata)
-  rownames(metadata) <- metadata$sampleID
-  #put it together
-  OTU = otu_table(amr_count_table, taxa_are_rows = TRUE)
-  TAX = tax_table(as.matrix(taxa_short))
-  META = sample_data(metadata)
-  ps <- phyloseq(OTU, TAX, META)
+  ps <- amr_raw_to_phyloseq(path.to.amr.files, metadata, coveragenumber, keepSNP)
   phyloseq_to_metagenomeSeq(ps)
 }
