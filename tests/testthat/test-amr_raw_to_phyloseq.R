e0365b715a16a94961154199d62ba3481e9fc4c9
Quentin Read <quentin.read@usda.gov>
2022-05-16 12:47:49 -0400

    Tests covering all functions

--- tests/testthat/test-amr_raw_to_phyloseq.R
+++
@@ -1,7 +0,0 @@
-library(epi2me2r)
-test_that("amr_raw_to_phyloseq gives phyloseq object", {
-  metadata <- read.csv("testdata/example_metadata.csv")
-  df.test <- amr_raw_to_phyloseq(path.to.amr.files = "testdata/example_amr_data/",
-                                 metadata = metadata)
-  expect_s4_class(df.test, "phyloseq")
-})
